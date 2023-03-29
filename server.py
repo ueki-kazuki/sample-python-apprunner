98fe58a59b0a85f120223afa730c7a1e0fd50360
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2023-03-29 10:19:08 +0900

    Run server by 'flask' command

--- server.py
+++ server.py
@@ -7,7 +7,3 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return render_template('server.html', env=os.environ)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=PORT)
